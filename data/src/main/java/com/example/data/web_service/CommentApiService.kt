9bc9dd5e9c26190a4f81977334c9ab202e96bbc1
AbdullhGaber <abdullh760@yahoo.com>
2026-02-01 02:49:09 +0200

    + Add CommentApiService

---
+++ data/src/main/java/com/example/data/web_service/CommentApiService.kt
@@ -0,0 +1,9 @@
+package com.example.data.web_service
+
+import com.example.data.model.CommentResponse
+import retrofit2.http.GET
+
+interface CommentApiService {
+    @GET("/comments")
+    suspend fun getComments() : CommentResponse
+}
