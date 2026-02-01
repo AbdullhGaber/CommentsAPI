eb6d5aa0fdd92b7a30f2dd001bc22b461bf9b757
AbdullhGaber <abdullh760@yahoo.com>
2026-02-01 02:48:32 +0200

    + Add CommentDTO

---
+++ data/src/main/java/com/example/data/model/CommentDTO.kt
@@ -0,0 +1,10 @@
+package com.example.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CommentDTO(
+    val id: Int,
+    @SerializedName("name")
+    val title: String,
+    val body: String,
+)
