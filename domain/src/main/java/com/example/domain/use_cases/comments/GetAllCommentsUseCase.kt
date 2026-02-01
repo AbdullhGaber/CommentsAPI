3bd709792cc9fc51d9765d87dc0be384114cb2d4
AbdullhGaber <abdullh760@yahoo.com>
2026-02-01 02:45:32 +0200

    - Remove onSuccess/onFailure callbacks from getAllComments in GetAllCommentsUseCase

--- domain/src/main/java/com/example/domain/use_cases/comments/GetAllCommentsUseCase.kt
+++ domain/src/main/java/com/example/domain/use_cases/comments/GetAllCommentsUseCase.kt
@@ -2,14 +2,13 @@ package com.example.domain.use_cases.comments
 
 import com.example.domain.entity.Comment
 import com.example.domain.repositories.comment.CommentRepository
+import com.example.domain.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
 class GetAllCommentsUseCase(
     private val commentRepository: CommentRepository
 ){
-    operator fun invoke(
-        onSuccess : (List<Comment>) -> Unit,
-        onFailure : (Throwable) -> Unit
-    ){
-        commentRepository.getAllComments(onSuccess, onFailure)
+    operator fun invoke() : Flow<Resource<List<Comment>>>{
+        return commentRepository.getAllComments()
     }
 }
